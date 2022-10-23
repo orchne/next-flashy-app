b9a7b7d4999fe68d441a4475b114ad17efe160a2
unknown <muhammad20149@mail.unpad.ac.id>
2022-10-23 10:28:35 +0700

    fix: [SOLVED] implement storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,6 @@
 // @ts-check
-import { RouterContext } from "next/dist/shared/lib/router-context";
 import * as NextImage from "next/image";
-import "../pages/globals.css";
+import "../styles/globals.css";
 
 const BREAKPOINTS_INT = {
   xs: 375,
