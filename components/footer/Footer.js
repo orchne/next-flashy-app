35e68ff6a676a3c957c81bea6898c1decbff25fc
unknown <muhammad20149@mail.unpad.ac.id>
2022-10-27 22:21:56 +0700

    style: add footer style

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer aria-label="Site Footer" class="bg-white">
@@ -60,68 +62,67 @@ const Footer = () => {
         <nav aria-label="Footer Nav" class="mt-12">
           <ul class="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12 font-heading">
             <li>
-              <a
-                class="text-gray-700 transition hover:text-gray-700/75"
+              <Link
+                className="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 About
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 class="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Careers
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 class="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 History
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 class="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Services
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 class="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Projects
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 class="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Blog
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
 
         <ul class="mt-12 flex justify-center gap-6 md:gap-8">
-          <li>
-            <a
-              href="/"
+          <Link href="/">
+            <li
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:text-gray-700/75"
+              className="text-gray-700 transition hover:text-gray-700/75 hover:cursor-pointer"
             >
               <span class="sr-only">Facebook</span>
               <svg
@@ -136,15 +137,14 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-            </a>
-          </li>
+            </li>
+          </Link>
 
-          <li>
-            <a
-              href="/"
+          <Link href="/">
+            <li
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:text-gray-700/75"
+              class="text-gray-700 transition hover:text-gray-700/75 hover:cursor-pointer"
             >
               <span class="sr-only">Instagram</span>
               <svg
@@ -159,15 +159,14 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-            </a>
-          </li>
+            </li>
+          </Link>
 
-          <li>
-            <a
-              href="/"
+          <Link href="/">
+            <li
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:text-gray-700/75"
+              class="text-gray-700 transition hover:text-gray-700/75 hover:cursor-pointer"
             >
               <span class="sr-only">Twitter</span>
               <svg
@@ -178,15 +177,14 @@ const Footer = () => {
               >
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
-            </a>
-          </li>
+            </li>
+          </Link>
 
-          <li>
-            <a
-              href="/"
+          <Link href="/">
+            <li
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:text-gray-700/75"
+              class="text-gray-700 transition hover:text-gray-700/75 hover:cursor-pointer"
             >
               <span class="sr-only">GitHub</span>
               <svg
@@ -201,15 +199,14 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-            </a>
-          </li>
+            </li>
+          </Link>
 
-          <li>
-            <a
-              href="/"
+          <Link href="/">
+            <li
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:text-gray-700/75"
+              className="text-gray-700 transition hover:text-gray-700/75 hover:cursor-pointer"
             >
               <span class="sr-only">Dribbble</span>
               <svg
@@ -224,8 +221,8 @@ const Footer = () => {
                   clip-rule="evenodd"
                 />
               </svg>
-            </a>
-          </li>
+            </li>
+          </Link>
         </ul>
       </div>
     </footer>
