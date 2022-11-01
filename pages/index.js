167d1f928705390e69a22d6456acfeedf2a1dbaa
unknown <muhammad20149@mail.unpad.ac.id>
2022-11-02 01:07:14 +0700

    feat: implement react-tiny-fab and add FAB component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import HeroSection from "@components/header/hero/HeroSection";
 import ExploreTags from "@components/header/explore-tags-section/ExploreTags";
-import FloatingActionButton from "@components/floating-action-button/FloatingActionButton";
+// import FloatingActionButton from "@components/floating-action-button/FloatingActionButton";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <ExploreTags />
-      <FloatingActionButton />
+      {/* <FloatingActionButton /> */}
     </>
   );
 };
